78c1b44c6a8c39cd5daeef772b02dffc342e2a2e
Julio Cesar Ody <julioody@gmail.com>
2008-10-13 14:17:57 +1100

    :)

---
+++ README.markdown
@@ -0,0 +1,20 @@
+#Schmoones
+
+Inspired by [Choones](http://github.com/benschwarz/choones/tree/master), this started off as a [Prototype](http://www.prototypejs.org/) port,
+but I ended up adding message queueing and transitions to it.
+
+Refer to [Choones' README](http://github.com/benschwarz/choones/tree/master) for more info about the pattern.
+
+##Usage
+
+Refer to the global SCHMOONES object that gets declared when you initialize a page:
+
+    SCHMOONES.message({title: "Hola", message: "Mundo"})
+
+##Styling
+
+Schmoones comes with default settings for padding, margins, and sizes. You should be able to override them with a CSS file, or messing with the code.
+
+##Oh but...
+
+Yeah, it doesn't feature success or failure messages. I had no need for any of that, so I left it out. Specifying how long a message lasts on the screen is something I'm implement soon.
---
+++ schmoones.js
@@ -0,0 +1,69 @@
+var Schmoones = Class.create();
+
+Schmoones.prototype = {  
+  initialize: function() {
+    this.container = new Element('div', { id: 'schmoones', style: 'position: absolute; left: 0; right: 0; bottom: 0; height: 70px; background-color: #000; color: #fff; filter:alpha(opacity=85); -moz-opacity: 0.85; opacity: 0.85; display: none; ' });
+    this.container.update('<div><h3></h3><p></p></div>');
+    document.body.appendChild(this.container);
+    this.queue = [];
+    this.expanded = false;
+    this.stylize();
+    this.startEngine();
+    document.observe('scroll', this.scrollingHandler);
+  },
+  
+  stylize: function() {
+    this.container.down('div h3').setStyle({margin: '10px', padding: 0});
+    this.container.down('div p').setStyle({margin: '5px 10px', padding: 0});
+  },
+  
+  scrollingHandler: function() {    
+    var clip = document.viewport.getScrollOffsets()[1];
+    $('schmoones').setStyle({bottom: '-' + clip + 'px'});
+  },
+  
+  message: function(msg) {
+    this.queue.push(function() {
+      if(!this.expanded) {
+        new Effect.BlindDown('schmoones', { queue: { position: 'front', scope: 'schmoones' } } );
+        this.expanded = true;
+      }
+      new Effect.Fade(this.container.down('div'), { duration: 0.2 });
+      setTimeout(function() {
+        $$('#schmoones div h3')[0].update(msg.title);
+        $$('#schmoones div p')[0].update(msg.message);              
+        new Effect.Appear($$('#schmoones div')[0], { duration: 0.2 });
+      }, 200)
+    })
+  },
+  
+  vacant: function() {
+    if (!this.executing || (this.executing && ( (new Date().getTime() - this.started) > 5000 ))) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  
+  tick: function(schmoones) {
+    if (schmoones.vacant()) {
+      if (schmoones.queue.size() == 0) {
+        if (schmoones.expanded) new Effect.BlindUp('schmoones', { queue: { position: 'front', scope: 'schmoones' } } );
+        schmoones.expanded = false;
+        return false;
+      }
+      schmoones.executing = schmoones.queue.shift();
+      schmoones.started = new Date().getTime();
+      schmoones.executing();
+    } else {
+      return false;
+    }
+  },
+    
+  startEngine: function() {
+    var proxy = this;
+    this.engine = new PeriodicalExecuter(function() { proxy.tick(proxy) }, 1);
+  }
+}
+
+$(document).observe('dom:loaded', function() { window.SCHMOONES = new Schmoones() })
